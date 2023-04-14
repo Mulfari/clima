802963a3865aa9cb4009e082e970a51f29ffbe46
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-14 04:51:27 -0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import './App.css'
-import Weather from './components/Weather'
+import React, { useState, useEffect } from 'react';
+import Weather from './components/Weather';
+import './App.css';
 
-function App() {
+const App = () => {
+  const [background, setBackground] = useState('');
+
+  useEffect(() => {
+    const setAppBackground = () => {
+      const weatherBackground = document.querySelector('.App');
+      const weatherCode = background.slice(0, 2);
+
+      switch (weatherCode) {
+        case '01':
+        case '02':
+          weatherBackground.style.backgroundImage = 'url(/images/clear-sky.jpg)';
+          break;
+        case '03':
+        case '04':
+          weatherBackground.style.backgroundImage = 'url(/images/cloudy.jpg)';
+          break;
+        case '09':
+        case '10':
+          weatherBackground.style.backgroundImage = 'url(/images/rain.jpg)';
+          break;
+        case '11':
+          weatherBackground.style.backgroundImage = 'url(/images/thunderstorm.jpg)';
+          break;
+        case '13':
+          weatherBackground.style.backgroundImage = 'url(/images/snow.jpg)';
+          break;
+        case '50':
+          weatherBackground.style.backgroundImage = 'url(/images/mist.jpg)';
+          break;
+        default:
+          weatherBackground.style.backgroundImage = 'url(/images/default.jpg)';
+      }
+    };
+
+    setAppBackground();
+  }, [background]);
 
   return (
     <div className="App">
-      <Weather />
+      <Weather setBackground={setBackground} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,58 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Weather.css';
-import clear from './images/clear.jpg';
-import clouds from './images/clouds.jpg';
-import rain from './images/rain.jpg';
-import snow from './images/snow.jpg';
-import thunderstorm from './images/thunderstorm.jpg';
 
-const Weather = () => {
+const Weather = ({ setBackground }) => {
   const [weatherData, setWeatherData] = useState(null);
   const [temperatureUnit, setTemperatureUnit] = useState('metric');
   const [city, setCity] = useState('');
-  const [error, setError] = useState(false);
 
   useEffect(() => {
-    const getWeatherData = async () => {
-      const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      };
-      navigator.geolocation.getCurrentPosition(async (position) => {
-        const { latitude, longitude } = position.coords;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a391e33ab6494e0851324aca91e03228&units=${temperatureUnit}`;
+    const fetchWeatherData = async () => {
+      let url;
+      if (city === '') {
+        const options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        };
+        navigator.geolocation.getCurrentPosition(async (position) => {
+          const { latitude, longitude } = position.coords;
+          url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a391e33ab6494e0851324aca91e03228&units=${temperatureUnit}`;
+          const response = await axios.get(url);
+          setWeatherData(response.data);
+          setBackground(response.data.weather[0].icon);
+        }, () => {
+          console.log('Error getting position');
+        }, options);
+      } else {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a391e33ab6494e0851324aca91e03228&units=${temperatureUnit}`;
         const response = await axios.get(url);
         setWeatherData(response.data);
-      }, () => {
-        console.log('Error getting position');
-      }, options);
+        setBackground(response.data.weather[0].icon);
+      }
     };
 
-    getWeatherData();
-  }, [temperatureUnit]);
+    fetchWeatherData();
+  }, [city, temperatureUnit, setBackground]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setCity(e.target.city.value);
+  };
 
   const handleClick = () => {
     const newUnit = temperatureUnit === 'metric' ? 'imperial' : 'metric';
     setTemperatureUnit(newUnit);
-  };
-
-  const searchCity = async (e) => {
-    e.preventDefault();
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a391e33ab6494e0851324aca91e03228&units=${temperatureUnit}`;
-    try {
-      const response = await axios.get(url);
-      setWeatherData(response.data);
-      setError(false);
-    } catch (error) {
-      if (error.response.status === 404) {
-        setError(true);
-      } else {
-        console.log(error);
-      }
-    }
-    setCity('');
   };
 
   if (!weatherData) {
@@ -65,73 +56,47 @@ const Weather = () => {
   const { temp, humidity } = main;
   const { speed, deg } = wind;
 
-  const getBackgroundImage = (icon) => {
-    const weatherType = icon.slice(0, 2);
-    switch (weatherType) {
-      case '01':
-      case '02':
-        return `url(${clear})`;
-      case '03':
-      case '04':
-        return `url(${clouds})`;
-      case '09':
-      case '10':
-        return `url(${rain})`;
-      case '11':
-        return `url(${thunderstorm})`;
-      case '13':
-        return `url(${snow})`;
-      default:
-        return `url(${clear})`;
-    }
-  };
-
-  
-  const backgroundImage = getBackgroundImage(icon);
-
   return (
-    <div className="weather" style={{ backgroundImage }}>
+    <div className="weather">
       <div className="weather-header">
         <h2 className="weather-title">{name}, {country}</h2>
         <button className="weather-unit-button" onClick={handleClick}>
           {temperatureUnit === 'metric' ? '°C' : '°F'}
         </button>
       </div>
-      <form onSubmit={searchCity}>
+      <form onSubmit={handleSearch}>
         <input
           type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder="Enter city name"
+          name="city"
+          placeholder="Search city"
         />
         <button type="submit">Search</button>
       </form>
-      {error && <div className="alert">No se pudo encontrar la localidad</div>}
       <div className="weather-info">
         <div className="weather-icon">
           <img src={`http://openweathermap.org/img/w/${icon}.png`} alt={description} />
-</div>
-<div className="weather-temp">
-Temperature: {temp.toFixed(1)} {temperatureUnit === 'metric' ? '°C' : '°F'}
-</div>
-<div className="weather-desc">
-Description: {description}
-</div>
-<div className="weather-humidity">
-Humidity: {humidity}%
-</div>
-<div className="weather-wind">
-Wind: {speed} m/s, {deg}°
-</div>
-<div className="weather-pressure">
-Pressure: {main.pressure} hPa
-</div>
-<div className="weather-feels-like">
-Feels like: {main.feels_like.toFixed(1)} {temperatureUnit === 'metric' ? '°C' : '°F'}
-</div>
-</div>
-</div>
-);
+        </div>
+        <div className="weather-temp">
+          Temperature: {temp.toFixed(1)} {temperatureUnit === 'metric' ? '°C' : '°F'}
+        </div>
+        <div className="weather-desc">
+          Description: {description}
+        </div>
+        <div className="weather-humidity">
+          Humidity: {humidity}%
+        </div>
+        <div className="weather-wind">
+        Wind: {speed} m/s, {deg}°
+        </div>
+        <div className="weather-pressure">
+          Pressure: {main.pressure} hPa
+        </div>
+        <div className="weather-feels-like">
+          Feels like: {main.feels_like.toFixed(1)} {temperatureUnit === 'metric' ? '°C' : '°F'}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Weather;
