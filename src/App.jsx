d98193b2407a578b1eb3a8479520c3cdbd029239
Salvador Mulfari <josesalvador0102@gmail.com>
2023-04-14 05:02:11 -0400

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import Weather from './components/Weather';
-import './App.css';
+import './App.css'
+import Weather from './components/Weather'
 
-const App = () => {
-  const [background, setBackground] = useState('');
-
-  useEffect(() => {
-    const setAppBackground = () => {
-      const weatherBackground = document.querySelector('.App');
-      const weatherCode = background.slice(0, 2);
-
-      switch (weatherCode) {
-        case '01':
-        case '02':
-          weatherBackground.style.backgroundImage = 'url(/images/clear-sky.jpg)';
-          break;
-        case '03':
-        case '04':
-          weatherBackground.style.backgroundImage = 'url(/images/cloudy.jpg)';
-          break;
-        case '09':
-        case '10':
-          weatherBackground.style.backgroundImage = 'url(/images/rain.jpg)';
-          break;
-        case '11':
-          weatherBackground.style.backgroundImage = 'url(/images/thunderstorm.jpg)';
-          break;
-        case '13':
-          weatherBackground.style.backgroundImage = 'url(/images/snow.jpg)';
-          break;
-        case '50':
-          weatherBackground.style.backgroundImage = 'url(/images/mist.jpg)';
-          break;
-        default:
-          weatherBackground.style.backgroundImage = 'url(/images/default.jpg)';
-      }
-    };
-
-    setAppBackground();
-  }, [background]);
+function App() {
 
   return (
     <div className="App">
-      <Weather setBackground={setBackground} />
+      <Weather />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
